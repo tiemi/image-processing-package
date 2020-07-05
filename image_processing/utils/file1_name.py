886d8a4e79b80a9df1bb39d61797d93ef71e6ffc
Karina Tiemi <karinat@take.net>
2020-07-05 11:04:59 -0300

    added plot_result function

--- image_processing/utils/file1_name.py
+++ image_processing/utils/file1_name.py
@@ -5,3 +5,15 @@ def plot_image(image):
     plt.imshow(image, cmap='gray')
     plt.axis('off')
     plt.show()
+
+def plot_result(*args):
+    number_images = len(args)
+    fig, axis = plt.subplots(nrows=1, ncols = number_images, figsize=(12, 4))
+    names_lst = ['Image {}'.format(i) for i in range(1, number_images)]
+    names_lst.append('Result')
+    for ax, name, image in zip(axis, names_lst, args):
+        ax.set_title(name)
+        ax.imshow(image, cmap='gray')
+        ax.axis('off')
+    fig.tight_layout()
+    plt.show()
