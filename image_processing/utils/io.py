4fcb9dbcc127a8dbf35d4ca6e02da00f71673bc1
Karina Tiemi <karinat@take.net>
2020-07-05 11:53:43 -0300

    added io functions

--- image_processing/utils/io.py
+++ image_processing/utils/io.py
@@ -1,5 +1,8 @@
-from skimage.io import imread
+from skimage.io import imread, imsave
 
 def read_image(path, is_gray = False):
     image = imread(path, as_gray = is_gray)
     return image
+
+def save_image(image, path):
+    imsave(path, image) 
