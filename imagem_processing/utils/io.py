cd0953ac9ca19407ac01526a3ad25a77e52aa96f
unknown <hanieri21@gmail.com>
2023-07-04 22:13:09 -0300

    meu pacote3

--- imagem_processing/utils/io.py
+++
@@ -1,8 +0,0 @@
-from skimage.io import imread, imsave
-
-def read_image(path, is_gray = False):
-    image = imread(path, as_gray = is_gray)
-    return image
-
-def save_image(image, path):
-    imsave(path, image) 
